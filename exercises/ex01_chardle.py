19f6d9b65c4c1e5ecad5dbdfa03aefd251148c1f
Sophia Romero <siromero@unc.edu>
2022-08-29 23:23:59 -0400

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -6,4 +6,14 @@ user_name: str = input("Enter a 5-character word: ")
 letter: str = input("Enter a single character: ")
 
 print("Searching for " + letter + " in " + user_name)
-print(letter + " found at index")
+
+if letter == user_name [0]:
+    print(letter + " found at index 0")
+if letter == user_name [1]:
+    print(letter + " found at index 1")
+if letter == user_name [2]:
+    print(letter + " found at index 2")
+if letter == user_name [3]:
+    print(letter + " found at index 3")
+if letter == user_name [4]:
+    print(letter + " found at index 4")
