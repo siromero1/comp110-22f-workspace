bbc281b866fc3015f829b0f011ebd61b1cd7e4b1
Sophia Romero <siromero@unc.edu>
2022-08-29 23:40:15 -0400

    Progress Exercise 01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -7,14 +7,14 @@ letter: str = input("Enter a single character: ")
 
 print("Searching for " + letter + " in " + user_name)
 
-if letter == user_name [0]:
+if letter == user_name[0]:
     print(letter + " found at index 0")
-if letter == user_name [1]:
+if letter == user_name[1]:
     print(letter + " found at index 1")
-if letter == user_name [2]:
+if letter == user_name[2]:
     print(letter + " found at index 2")
-if letter == user_name [3]:
+if letter == user_name[3]:
     print(letter + " found at index 3")
-if letter == user_name [4]:
+if letter == user_name[4]:
     print(letter + " found at index 4")
 
