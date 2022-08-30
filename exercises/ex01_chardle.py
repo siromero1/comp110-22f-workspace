36a64f3e3de16c0b5dbfb15fe8e227235bc48f01
Sophia Romero <siromero@unc.edu>
2022-08-30 01:12:28 -0400

    Progress on ex01

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -2,18 +2,39 @@
 
 __author__ = "730576249"
 
-user_name: str = input("Enter a 5-character word: ")
+word: str = input("Enter a 5-character word: ")
 letter: str = input("Enter a single character: ")
 
-print("Searching for " + letter + " in " + user_name)
+print("Searching for " + letter + " in " + word)
 
-if letter == user_name[0]:
+if letter == word[0]:
     print(letter + " found at index 0")
-if letter == user_name[1]:
+if letter == word[1]:
     print(letter + " found at index 1")
-if letter == user_name[2]:
+if letter == word[2]:
     print(letter + " found at index 2")
-if letter == user_name[3]:
+if letter == word[3]:
     print(letter + " found at index 3")
-if letter == user_name[4]:
+if letter == word[4]:
     print(letter + " found at index 4")
+
+count: int = len("word")
+
+if letter == count(0):
+    print("No instances of " + letter + "found in " + word)
+if letter == count(1):
+    print("1 instance of " + letter + "found in " + word)
+if letter == count(2):
+    print("2 instances of " + letter + "found in " + word)
+if letter == count(3):
+    print("3 instances of " + letter + "found in " + word)
+if letter == count(4):
+    print("4 instances of " + letter + "found in " + word)
+if letter == count(5):
+    print("5 instances of " + letter + "found in " + word)
+
+char: str
+if char < 5:
+    print("Error: Word must contain 5 characters")
+if char > 5:
+    print("Error: Word must conatin 5 characters")
