43d8339af70ff64aa6fd0c1d7287e0d06dcf114a
Sophia Romero <siromero@unc.edu>
2022-09-08 17:36:07 -0400

    Progress on exercise 2

---
+++ exercises/ex02_one_shot_wordle.py
@@ -0,0 +1,40 @@
+"""EX02 - One-Shot - Wordle."""
+
+__author__: str = 730576249
+
+secret_word: str = "python" # declaring the secret word "python"
+
+i: str = 0 # declaring varibales for while loops
+emoji: str = ""
+WHITE_BOX: str = "\U00002B1C"
+GREEN_BOX: str = "\U0001F7E9"
+YELLOW_BOX: str = "\U0001F7E8"
+
+
+guess: str = input("What is your 6-letter guess? ") # asking for user input
+
+while len(guess) != len("python"):
+    guess = input("That was not 6 letters! Try again: ")
+
+
+while i < len(guess): # while loop for emoji and indices
+    if guess[i] == secret_word[i]:
+        emoji += GREEN_BOX
+    else:
+        guess_exists: bool = False
+        alt_indicies: int = 0
+        while guess_exists == False and alt_indicies < len(secret_word):
+            if guess[i] == secret_word[alt_indicies]:
+                guess_exists = True
+            alt_indicies += 1
+        if guess_exists == True:
+            emoji += YELLOW_BOX
+        else:
+            emoji += WHITE_BOX
+    i += 1
+
+print(emoji) # printing result of matching indices as emojis
+if guess != secret_word:
+    print("Not quite. Play again soon! ")
+if guess == secret_word:
+    print("Woo! You got it!")
