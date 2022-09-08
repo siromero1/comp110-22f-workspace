9d09400d862c3b2d39020b2556aa1b09ad73f929
Sophia Romero <siromero@unc.edu>
2022-09-08 19:17:10 -0400

    progress on ex02

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -10,17 +10,16 @@ WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-
 guess: str = input("What is your 6-letter guess? ") # asking for user input
 
 while len(guess) != len("python"):
     guess = input("That was not 6 letters! Try again: ")
 
 
-while i < len(guess): # while loop for emoji and indices
+while i < len(guess): # matching letters in the same index will result in a green box
     if guess[i] == secret_word[i]:
         emoji += GREEN_BOX
-    else:
+    else: 
         guess_exists: bool = False
         alt_indicies: int = 0
         while guess_exists == False and alt_indicies < len(secret_word):
@@ -31,7 +30,8 @@ while i < len(guess): # while loop for emoji and indices
             emoji += YELLOW_BOX
         else:
             emoji += WHITE_BOX
-    i += 1
+    i += 1 #continue to increase index to move on to next letter in guess
+
 
 print(emoji) # printing result of matching indices as emojis
 if guess != secret_word:
