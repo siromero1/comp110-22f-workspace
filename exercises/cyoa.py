d321259afae4a7eb38dee6e4653f66c1a0048811
Sophia Romero <siromero@unc.edu>
2022-10-04 21:48:20 -0400

    progress6

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -36,7 +36,7 @@ def greet() -> None:
     print(f"Welcome {player}{HEART_EMOJI}\nThe goal is to guess the secret number is least amount of guesses possible.\nThe secret number is a randomly generated number 0 to 10.\nA point is deducted after each guess.")
 
 
-def users_guess_10(secret_number: int = randint(0, 10), total_points: int  = "") -> int:
+def users_guess_10(secret_number: int = randint(0, 10), total_points: int = "") -> int:
     """Easy difficulty game option."""
     compare_guess: int = int(input("What is your guess (0-10)? "))
     PARTY_EMOJI: str = "\U0001F973"
@@ -45,7 +45,7 @@ def users_guess_10(secret_number: int = randint(0, 10), total_points: int  = "")
     while points > 0:
         if compare_guess == secret_number:
             print(f"Winner{PARTY_EMOJI}! You correctly guessed the secret number!\nTotal points: {points}")
-            return points
+            return total_points
         else:
             compare_guess = int(input(f"Sorry {player}, that is incorrect. Guess again: "))
             if compare_guess > secret_number:
